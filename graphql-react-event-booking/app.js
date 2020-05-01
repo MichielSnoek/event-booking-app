90e80cff2c99e1a04afc6f672c1ec0372548710d
Lloyd Chambrier <lloydchambrier@gmail.com>
2020-05-01 17:47:24 +0200

    Updated resolvers to work with Event model

--- graphql-react-event-booking/app.js
+++ graphql-react-event-booking/app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser")
 const graphQlHttp = require("express-graphql")
 const { buildSchema } = require("graphql")
 const mongoose = require("mongoose")
+const Event = require("./models/event")
 app.use(bodyParser.json())
 
 const events = [];
@@ -36,24 +37,43 @@ app.use('/graphql', graphQlHttp({
     `),
     rootValue: {
         events: () => {
-            return events;
+           return Event.find().then(events =>{
+                return events.map(event =>{
+                    const {_id, title, description, date, price} = event._doc
+                    return {...event.doc, _id: _id.toString(),title, description, date, price}
+                })
+            })
+            .catch(e => console.log(e))
         },
         createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
+            // const event = {
+            //     _id: Math.random().toString(),
+            //     title: args.eventInput.title,
+            //     description: args.eventInput.description,
+            //     price: +args.eventInput.price,
+            //     date: args.eventInput.date
+            // }
+            const event = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
-                date: args.eventInput.date
-            }
-            events.push(event)
-            return event;
+                date: new Date(args.eventInput.date)
+            })
+           return event.save()
+            .then(result => {
+                console.log(result)
+                return {...result._doc};
+            })
+            .catch(e => {
+                console.log(e)
+            });      
         }
     },
     graphiql: true
 }))
 
 mongoose
-.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-ujyk4.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
+// .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-ujyk4.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
+.connect(`mongodb://localhost:27017/${process.env.MONGO_DB}`, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=> app.listen(3000, () => console.log('Event booking server listening on port 3000')))
 .catch(err => console.log(err))
