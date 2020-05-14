dcd6dea6ede6d29f8114b6b95bd5bf5e7c4538fe
Lloyd Chambrier <lloydchambrier@gmail.com>
2020-05-14 14:53:00 +0200

    Added query logic in Events component

--- graphql-react-event-booking/frontend/src/pages/Events.js
+++ graphql-react-event-booking/frontend/src/pages/Events.js
@@ -1,10 +1,51 @@
 import React, { Component } from 'react'
 
 export default class EventsPage extends Component {
+    state = {
+        eventsList: []
+    }
+    componentDidMount(){
+        let requestBody = {
+            query: `
+                query {
+                    events {
+                        title
+                        description
+                        price
+                        date
+                    }
+                }`
+        }
+
+        fetch('http://localhost:8000/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers:{
+                'Content-Type':'application/json'
+            }
+        })
+        .then(res => {
+            if(res.status !== 200){
+                throw new Error('Failed')
+            }
+            return res.json()
+        })
+        .then(resData => {
+            this.setState({eventsList: resData.data.events})
+        })
+    }
     render() {
         return (
             <div>
                 <h1>The Events Page</h1>
+                {this.state.eventsList ? this.state.eventsList.map((event, key) => (
+                    <div key={key}>
+                        <h3>{event.title}</h3>
+                        <p>{event.description}</p>
+                        <p>{event.price}$</p>
+                        <p>{event.date}</p>
+                    </div>
+                ))  : ''}
             </div>
         )
     }
